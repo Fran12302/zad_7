d330d6fbcc5f0c11adcd7a4499966b08c9fa86f5
Fran12302 <156135106+Fran12302@users.noreply.github.com>
2024-02-01 13:56:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿namespace iterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                PlayersCollection playersCollection= new PlayersCollection();
+            playersCollection.players.Add(new Player("BUKAYO", "SAKA"));
+            playersCollection.players.Add(new Player("RIYADH", "MAHREZ"));
+
+            PlayerIterator playeriterator=(PlayerIterator)playersCollection.CreateIterator();
+            Console.WriteLine($"{playeriterator.GetNext().Name}");
+            
+        }
+    }
+
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public Player(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+
+    public interface IIterator
+    {
+        public Player GetNext();
+        public bool HasNext();
+    }
+
+    public interface IIteratorCollection
+    {
+        public IIterator CreateIterator();
+    }
+
+    public class PlayerIterator : IIterator
+    {
+        PlayersCollection playersCollection;
+        int Current;
+
+        public PlayerIterator(PlayersCollection playersCollection)
+        {
+            this.playersCollection = playersCollection;
+            Current = 0;
+        }
+
+        public Player GetNext()
+        {
+            Player player = playersCollection.GetPlayer(Current);
+            Current++;
+            return player;
+        }
+
+        public bool HasNext()
+        {
+            return Current >= playersCollection.Count();
+        }
+    }
+
+    public class PlayersCollection : IIteratorCollection
+    {
+        public List<Player> players;
+        public PlayersCollection()
+        {
+            players = new List<Player>();
+        }
+        public IIterator CreateIterator()
+        {
+            return new PlayerIterator(this);
+        }
+        public Player GetPlayer(int index)
+        {
+            return players[index];
+        }
+        public int Count()
+        {
+            return players.Count;
+        }
+    }
+
+   
+
+}
---
+++ lanac.cs
@@ -0,0 +1,76 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata;
+
+namespace DZ_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player= new Player();
+            player.Name = "Cristiano Ronaldo";
+            player.Goals = 2;
+            IEvaluator Boto = new TransferManager();
+            IEvaluator Zekic = new Coach();
+            Boto.SetNext(Zekic);
+            Boto.Evaluate(player);
+        }
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Goals { get; set; }
+    }
+
+   
+    public interface IEvaluator
+    {
+        void Evaluate(Player player);
+        void SetNext(IEvaluator evaluator);
+    }
+
+    public abstract class Evaluator : IEvaluator
+    {
+        protected IEvaluator next;
+        public abstract void Evaluate(Player player);
+        public void SetNext(IEvaluator evaluator) { this.next = evaluator; }
+    }
+
+    public class TransferManager : Evaluator
+    {
+        public override void Evaluate(Player player)
+        {
+           
+
+            if (player.Goals < 5)
+            {
+                 next.Evaluate(player);
+            }
+
+            else
+            {
+                Console.WriteLine("igrač kupljen, odlukom transfer managera");
+            }
+        }
+
+
+    }
+
+    public class Coach : Evaluator
+    {
+        public override void Evaluate(Player player)
+        {
+            if(player.Goals<5&&player.Name!="Cristiano Ronaldo")
+            {
+                Console.WriteLine("Igrač odbijen od strane kluba");
+            }
+
+            if(player.Goals>5||player.Name=="Cristiano Ronaldo")
+            {
+                Console.WriteLine("Igrač kupljen, odlukom trenera");
+            }
+        }
+    }
+
+}
